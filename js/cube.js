e53f8d2252be9e3fa27c7399b7cb8b12cac5fb64
Sea-i <144750241+Sea-i@users.noreply.github.com>
2023-10-30 12:24:09 +0900

    Add files via upload

--- js/cube.js
+++ js/cube.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
-    $("#logo a").on("mouseenter focus" ,function(){
-        $("#header-menu").stop().slideDown();
-        $("#header-menu ul").css({ "height" : "430px" , "padding-top" : "80px" });
-        $("#header-menu").css("top", "0" );
+    // console.log( $("#header-menu").height() );
+    $("#header-menu").on("mouseenter focus", function(){
+        $(".header-menu-sub").stop().slideDown(500);
+        $(this).css("height" , "380px");
     });
-    $(".header-menu-title").on("mouseenter focus" , function(){
+    $("#header-menu").mouseleave(function(){
+        $(".header-menu-sub").stop().slideUp(10);
+        $("#header-menu a").removeClass("ent");
+        $(this).stop().css("height","80px");
+    });
+    $("#header-menu").blur(function(){
+        $(".header-menu-sub").stop().slideUp(10);
+        $("#header-menu a").removeClass("ent");
+        $(this).css("height","80px");
+    });
+    $(".header-menu-title").on("mouseenter focus", function(){
         $(".header-menu-title").removeClass("ent");
+        $(".header-menu-sub-list").removeClass("ent");
         $(this).addClass("ent");
     });
-    $(".header-menu-sub-list").on("mouseenter focus" , function(){
+    $(".header-menu-sub-list").on("mouseenter focus", function(){
+        $(".header-menu-sub-list").removeClass("ent");
+        $(this).addClass("ent");
         $(".header-menu-title").removeClass("ent");
         $(this).parent().prev().addClass("ent");
     });
-    $("#header-menu").mouseleave(function(){
-        $("#header-menu a").removeClass("ent");
-        $("#header-menu").slideUp();
+
+    // << 테마 소개 JS >>
+    $(".theme-info img").on("mouseenter focus" , function(){
+        $(".theme-txt").stop().fadeOut();
+        $(this).next().fadeIn().css("display","flex");
     });
-    $("#header-menu").blur(function(){
-        $("#header-menu a").removeClass("ent");
-        $("#header-menu").slideUp();
+    $(".theme-info").mouseleave(function(){
+        $(".theme-txt").stop().fadeOut();
+    });
+    $(".theme-info").blur(function(){
+        $(".theme-txt").stop().fadeOut();
     });
 });//END ALL
